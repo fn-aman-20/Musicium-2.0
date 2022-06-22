b2b1e771fe39aa8056860dcc2435138ba7ea5234
Aman <99836388+fn-aman-20@users.noreply.github.com>
2022-06-22 11:11:43 +0530

    Create forward.js

---
+++ commands/🎧 Song/forward.js
@@ -0,0 +1,106 @@
+const {
+	MessageEmbed,
+	Message
+} = require("discord.js");
+const config = require("../../botconfig/config.json");
+const ee = require("../../botconfig/embed.json");
+const settings = require("../../botconfig/settings.json");
+const {
+	check_if_dj
+} = require("../../handlers/functions")
+module.exports = {
+	name: "forward", //the command name for the Slash Command
+	category: "🎧 Song",
+	usage: "forward <duration_in_secs>",
+	aliases: ["fwd"],
+	description: "Forwards the song by given number of seconds", //the command description for Slash Command Overview
+	cooldown: 10,
+	requiredroles: [], //Only allow specific Users with a Role to execute a Command [OPTIONAL]
+	alloweduserids: [], //Only allow specific Users to execute a Command [OPTIONAL]
+	run: async (client, message, args) => {
+		try {
+			//things u can directly access in an interaction!
+			const {
+				member,
+				channelId,
+				guildId,
+				applicationId,
+				commandName,
+				deferred,
+				replied,
+				ephemeral,
+				options,
+				id,
+				createdTimestamp
+			} = message;
+			const {
+				guild
+			} = member;
+			const {
+				channel
+			} = member.voice;
+			if (!channel) return message.reply({
+				embeds: [
+					new MessageEmbed().setColor(ee.wrongcolor).setTitle(`${client.allEmojis.x} **Join ${guild.me.voice.channel ? "my" : "a"} Voice Channel First!**`)
+				],
+
+			})
+			if (channel.guild.me.voice.channel && channel.guild.me.voice.channel.id != channel.id) {
+				return message.reply({
+					embeds: [new MessageEmbed()
+						.setColor(ee.wrongcolor)
+						.setFooter(ee.footertext, ee.footericon)
+						.setTitle(`${client.allEmojis.x} Join my Voice Channel!`)
+						.setDescription(`<#${guild.me.voice.channel.id}>`)
+					],
+				});
+			}
+			try {
+				let newQueue = client.distube.getQueue(guildId);
+				if (!newQueue || !newQueue.songs || newQueue.songs.length == 0) return message.reply({
+					embeds: [
+						new MessageEmbed().setColor(ee.wrongcolor).setTitle(`${client.allEmojis.x} **There's nothing being played right now!**`)
+					],
+
+				})
+
+				if (!args[0]) {
+					return message.reply({
+						embeds: [
+							new MessageEmbed()
+							.setColor(ee.wrongcolor)
+							.setFooter(ee.footertext, ee.footericon)
+							.setTitle(`${client.allEmojis.x} **Please mention a Forward Duration!**`)
+							.setDescription(`**Usage**\n\`${client.settings.get(guild.id, "prefix")}forward <duration_in_secs>\``)
+						],
+					})
+				}
+				let seekNumber = Number(args[0])
+				let seektime = newQueue.currentTime + seekNumber;
+				if (seektime >= newQueue.songs[0].duration) seektime = newQueue.songs[0].duration - 1;
+				if (check_if_dj(client, member, newQueue.songs[0])) {
+					return message.reply({
+						embeds: [new MessageEmbed()
+							.setColor(ee.wrongcolor)
+							.setFooter(ee.footertext, ee.footericon)
+							.setTitle(`${client.allEmojis.x} **You're not a DJ, nor the Song Requester!**`)
+							.setDescription(`**DJ ROLES**\n${check_if_dj(client, member, newQueue.songs[0])}`)
+						],
+					});
+				}
+				await newQueue.seek(seektime);
+				message.reply({
+					embeds: [new MessageEmbed()
+					  .setColor(ee.color)
+					  .setTimestamp()
+					  .setTitle(`⏩ **Forwarded the song by ${seekNumber} Seconds!**`)
+					  .setFooter(`Requested by ${member.user.tag}`, member.user.displayAvatarURL({dynamic: true}))]
+				})
+			} catch (e) {
+				console.log(e.stack ? e.stack : e)
+			}
+		} catch (e) {
+			console.log(String(e.stack).bgRed)
+		}
+	}
+}
