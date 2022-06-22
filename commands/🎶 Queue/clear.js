558b9bc26760e9e580a92988ca479f8842c090ba
Aman <99836388+fn-aman-20@users.noreply.github.com>
2022-06-22 11:14:37 +0530

    Create clear.js

---
+++ commands/🎶 Queue/clear.js
@@ -0,0 +1,103 @@
+const {
+	MessageEmbed,
+	Message
+} = require("discord.js");
+const config = require("../../botconfig/config.json");
+const ee = require("../../botconfig/embed.json");
+const settings = require("../../botconfig/settings.json");
+const {
+	check_if_dj
+} = require("../../handlers/functions")
+module.exports = {
+	name: "clearq", //the command name for the Slash Command
+
+	category: "🎶 Queue",
+	aliases: ["clearqueue", "cleanqueue", "emptyqueue"],
+	usage: "clear",
+
+	description: "Clears the Queue", //the command description for Slash Command Overview
+	cooldown: 10,
+	requiredroles: [], //Only allow specific Users with a Role to execute a Command [OPTIONAL]
+	alloweduserids: [], //Only allow specific Users to execute a Command [OPTIONAL]
+	run: async (client, message, args) => {
+		try {
+			//things u can directly access in an interaction!
+			const {
+				member,
+				channelId,
+				guildId,
+				applicationId,
+				commandName,
+				deferred,
+				replied,
+				ephemeral,
+				options,
+				id,
+				createdTimestamp
+			} = message;
+			const {
+				guild
+			} = member;
+			const {
+				channel
+			} = member.voice;
+			if (!channel) return message.reply({
+				embeds: [
+					new MessageEmbed().setColor(ee.wrongcolor).setTitle(`${client.allEmojis.x} **Join ${guild.me.voice.channel ? "my" : "a"} Voice Channel First!**`)
+				],
+
+			})
+			if (channel.guild.me.voice.channel && channel.guild.me.voice.channel.id != channel.id) {
+				return message.reply({
+					embeds: [new MessageEmbed()
+						.setColor(ee.wrongcolor)
+						.setFooter(ee.footertext, ee.footericon)
+						.setTitle(`${client.allEmojis.x} Join my Voice Channel!`)
+						.setDescription(`<#${guild.me.voice.channel.id}>`)
+					],
+				});
+			}
+			try {
+				let newQueue = client.distube.getQueue(guildId);
+				if (!newQueue || !newQueue.songs || newQueue.songs.length == 0) return message.reply({
+					embeds: [
+						new MessageEmbed().setColor(ee.wrongcolor).setTitle(`${client.allEmojis.x} **There's nothing being played right now!**`)
+					],
+
+				})
+				if (check_if_dj(client, member, newQueue.songs[0])) {
+					return message.reply({
+						embeds: [new MessageEmbed()
+							.setColor(ee.wrongcolor)
+							.setFooter(ee.footertext, ee.footericon)
+							.setTitle(`${client.allEmojis.x} **You're not a DJ, nor the Song Requester!**`)
+							.setDescription(`**DJ ROLES**\n${check_if_dj(client, member, newQueue.songs[0])}`)
+						],
+					});
+				}
+				let amount = newQueue.songs.length - 2;
+				newQueue.songs = [newQueue.songs[0]];
+				message.reply({
+					embeds: [new MessageEmbed()
+					  .setColor(ee.color)
+					  .setTimestamp()
+					  .setTitle(`🗑 **Cleared the Queue and deleted ${amount} Songs!**`)
+					  .setFooter(`Requested by ${member.user.tag}`, member.user.displayAvatarURL({dynamic: true}))]
+				})
+			} catch (e) {
+				console.log(e.stack ? e.stack : e)
+			}
+		} catch (e) {
+			console.log(String(e.stack).bgRed)
+		}
+	}
+}
+/**
+ * @INFO
+ * Bot Coded by Tomato#6966 | https://github.com/Tomato6966/Discord-Js-Handler-Template
+ * @INFO
+ * Work for Milrato Development | https://milrato.eu
+ * @INFO
+ * Please mention Him / Milrato Development, when using this Code!
+ * @INFO
+ */
